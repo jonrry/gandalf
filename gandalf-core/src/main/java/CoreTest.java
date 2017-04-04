ba366af1db554dd1114468b63e5d6d4c25d5eb88
zongyue.xzy <xiezongyue0811@gmail.com>
2017-04-04 17:12:06 +0800

    init

---
+++ gandalf-core/src/main/java/CoreTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zongyue.xzy on 17/4/4.
+ *
+ * @author zongyue.xzy
+ * @date 2017/04/04
+ */
+public class CoreTest {
+
+    public static void main(String[] rags){
+        System.out.print("hello gandalf");
+    }
+}
