ab5ddc04ff3d5884160d98005e1e820d5fc85c27
zongyue.xzy <xiezongyue0811@gmail.com>
2017-04-04 17:49:58 +0800

    init

---
+++ gandalf-core/src/main/java/RateLimiterUtil.java
@@ -0,0 +1,13 @@
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * Created by zongyue.xzy on 17/4/4.
+ *
+ * @author zongyue.xzy
+ * @date 2017/04/04
+ */
+public class RateLimiterUtil {
+
+
+    RateLimiter rateLimiter ;
+}
